2cb1e0a429d232c01f80d2f8a1470339b285935d
sugisuartanaya <sugisuartanaya@gmail.com>
2024-02-11 21:02:19 +0800

    create tabel and controller transaksi

---
+++ database/migrations/2024_02_11_204503_create_transaksis_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transaksis', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('id_pembeli');
+            $table->foreignId('id_penawaran');
+            $table->enum('status', ['pending', 'success', 'verified'])->default('pending');
+            $table->datetime('tanggal');
+            $table->text('foto_bukti');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transaksis');
+    }
+};
