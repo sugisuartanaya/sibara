6adade5f1af5c0042b56c402ff7ee1062107e7fd
sugisuartanaya <sugisuartanaya@gmail.com>
2024-03-09 14:02:00 +0800

    add icon website

--- resources/views/dashboard/layouts/main.blade.php
+++ resources/views/dashboard/layouts/main.blade.php
@@ -6,8 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>SIBARA | Kejaksaan Negeri Denpasar</title>
 
+    <link rel="icon" type="image/png" href="{{ asset('images/logo2.png') }}">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
-    {{-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/font-awesome@4.7.0/css/font-awesome.min.css"> --}}
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 
   </head>
