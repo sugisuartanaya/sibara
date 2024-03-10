cad16d85ccb9e9c473669b47939926d9fd05a048
sugisuartanaya <sugisuartanaya@gmail.com>
2024-03-11 03:25:17 +0800

    add popup whatsapp

--- resources/views/dashboard/layouts/main.blade.php
+++ resources/views/dashboard/layouts/main.blade.php
@@ -193,8 +193,35 @@
       </div>
     </div>
 
+    <div id="contact"></div>
+
+    <link rel="stylesheet" type="text/css" href="https://kejari-denpasar.kejaksaan.go.id/assets/css/jquery.contactus.min.css">
+    <style>
+    .arcontactus-widget .arcontactus-prompt, .arcontactus-widget .messangers-block {
+        width: 350px !important;
+    }
+    </style>
+
       
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+
+    <script src="{{ asset('js/contact.min.js') }}"></script>
+    <script>
+        window.addEventListener('load', function(){
+            $('#contact').contactUs({
+                align: 'right',
+                reCaptcha: true,
+                reCaptchaKey: '6Ldmw7EUAAAAAPFpUQb-zuhMRUQBx7VOW0bQpKKt',
+                theme: '#008749',
+                items: [{
+                            title: 'Call Center Admin SIPBARAN',
+                            icon: "<i class='fa fa-whatsapp'></i>",
+                            href: 'https://api.whatsapp.com/send?phone=6287796384230',
+                            color: '#4EB625'
+                        }]
+            });
+        });
+    </script>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     
